096e1c58bfe951880a80ddca539cbb2318371575
SHIVADHARSHINISATHISH <nisirtha29@gmail.com>
2016-05-30 23:12:12 -0700

    number.js file added

---
+++ number.js
@@ -0,0 +1,16 @@
+
+<script type="text/javascript">
+	var x=0;
+	if (x>0){
+		
+		document.write("The number is positive");
+	}
+	else if (x<0) {
+		
+		document.write("The number is negative");
+	}
+	else{
+        document.write("The number is zero");
+	}
+
+</script>
