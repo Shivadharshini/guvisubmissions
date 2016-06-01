c73da1be260ab509c35959f1cc26e31eb779af0c
SHIVADHARSHINISATHISH <nisirtha29@gmail.com>
2016-06-01 04:49:09 -0700

    odd.js added

---
+++ odd.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>Odd or Even number</title>
+</head>
+<body>
+<script type="text/javascript">
+for (i = 0; i<5; i++) {
+	document.write(i);
+	if(i%2==0){
+document.write("the Number is even<br>");
+}else
+{
+	document.write("the Number is odd");
+}
+}
+</script>
+</body>
+</html>
