2bea8d78deabfbc87f1d0e06af43c7c8eefbc25c
SHIVADHARSHINISATHISH <nisirtha29@gmail.com>
2016-06-01 05:08:25 -0700

    integer.js is added

---
+++ integer.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Count of integer</title>
+</head>
+<body>
+<script type="text/javascript">
+function getlength(number) {
+    document.write(number.toString().length);
+
+}
+var number=prompt('Enter a number');
+getlength(number);
+
+</script>
+</body>
+</html>
