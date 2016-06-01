d7a4346c7bab071748aca6575d1c358b34293430
SHIVADHARSHINISATHISH <nisirtha29@gmail.com>
2016-05-31 22:36:36 -0700

    hello.js file now added

---
+++ hello.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello</title>
+</head>
+<body>
+<script type="text/javascript">
+var i="hello";
+	for (var i = 0; i <5; i++) {
+	document.write("Hello<br>")
+	}
+</script>
+</body>
+</html>
