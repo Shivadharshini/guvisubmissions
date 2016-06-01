e03e4a185f6f0b14a6031ba96aab1122e1279dfc
SHIVADHARSHINISATHISH <nisirtha29@gmail.com>
2016-06-01 05:05:32 -0700

    natural.js is added

---
+++ natural.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Natural number</title>
+	<script type="text/javascript">
+var answer=0;
+var start_value=1;
+var end_value=10;
+for(start_value;start_value<end_value;start_value++){
+	answer=answer+start_value;
+}
+document.write("answer=" + answer);
+</script>
+</head>
+<body>
+</body>
+</html>
