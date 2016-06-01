fc4e5ecb6b26d2e680fdc133ecd0a1146002b6ba
SHIVADHARSHINISATHISH <nisirtha29@gmail.com>
2016-06-01 04:52:19 -0700

    vowel.js added

---
+++ vowel.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vowel</title>
+</head>
+<body>
+<script type="text/javascript">
+ var x;
+x=prompt("please enter the char");
+if (x=='a' || x=='e' || x=='i' || x=='o' ||x=='u' ||x=='A' ||x=='E' || x=='I' ||x=='O' || x=='U') {
+	document.write("The letter is vowel");
+}else
+{
+	document.write("The letter is consonant");
+} 
+</script>
+</body>
+</html>
