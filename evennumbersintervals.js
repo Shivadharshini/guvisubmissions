2b8f279d20ad56c43986f9ec61eba61c6d42fc8c
SHIVADHARSHINISATHISH <nisirtha29@gmail.com>
2016-06-08 23:24:47 -0700

    evennumbersintervals.js is added

---
+++ evennumbersintervals.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Even numbers between two intervals</title>
+</head>
+<body>
+<script type="text/javascript">
+	function getNumbers(start,end){
+		var i, start, end;
+		for(i=start; i<=end;i++){
+			if(i%2==0){
+				document.write(i+",");
+				
+			}
+
+			
+		}
+	}
+	var start=prompt('Enter a first number');
+	var end=prompt('Enter an end number');
+    getNumbers(start,end);
+</script>
+</body>
+</html>
