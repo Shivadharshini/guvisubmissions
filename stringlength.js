5c88d7fb688ddc664a10bbc333730a3532fe49fd
SHIVADHARSHINISATHISH <nisirtha29@gmail.com>
2016-06-09 22:38:39 -0700

    stringlength.js is added

---
+++ stringlength.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Count of integer</title>
+</head>
+<body>
+<script type="text/javascript">
+function getLength(string) {
+ document.write(string.toString().length);
+}
+var string=prompt('Enter a string');
+getLength(string);
+</script>
+</body>
+</html>
