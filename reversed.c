78eded1244342312a008c65d01c42624cce86a81
Shivadharshini <shivadharshinibabu@gmail.com>
2016-07-26 05:02:26 -0700

    reversed.c is added

---
+++ reversed.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char arr[100];
+printf("Enter a string to reversed:\n");
+gets(arr);
+strrev(arr);
+printf("Reverse of entered string is\n%s\n",arr);
+return 0;
+}
